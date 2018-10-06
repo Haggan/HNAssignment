b1ee158d04febf36594aa1c5d26796d4e22395e8
Robert Hagklint <robert.hagklint@gmail.com>
2018-10-06 19:47:07 +0200

    Expanded the result.json to contain more ads, and implemented some basic resizing of thumbnails

--- HNAssignment/Services/Concrete/HemnetImageCache.swift
+++ HNAssignment/Services/Concrete/HemnetImageCache.swift
@@ -25,7 +25,6 @@ extension HemnetImageCache: ImageCache {
         // If image already exist in the image cache, execute the closure with that image
         if let image = images[urlString] {
             completion(image, urlString)
-            return
         }
         
         // Else, we download the image and store the image
@@ -37,10 +36,36 @@ extension HemnetImageCache: ImageCache {
                 // placeholder instead and save it
             }
             
+            // Ensure that the image is not too big. Maybe the real estate agent uploaded a HUUUUGE
+            // file as thumbnail. We save the resized image in the images "cache" so that we don't waste
+            // runtime memory
+            image = image?.resize(targetWidth: 300)
+            
             self?.images[urlString] = image // It's possible that this is not enteriely thread safe. But
             // due to the 4 hour time constraint of the assignment, I won't put work into fixing it. I'm simply
             // acknowledging it :)
             completion(image, urlString)
         }
+    }
+}
+
+extension UIImage {
+    public func resize(targetWidth: CGFloat) -> UIImage {
+        let currentSize = self.size
+        
+        if currentSize.width <= targetWidth { return self }
+        
+        let widthRatio = targetWidth  / currentSize.width
+        let newSize = CGSize(width: currentSize.width * widthRatio,  height: currentSize.height * widthRatio)
+        
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        self.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage! // We want to crash here if this doesn't work, since something is seriously wrong
+        // if we can't simply resize the image
     }
 }
